3bd7c2da13872112ab7665151847dd9ed58e0fa7
Neelam Soni <neemssoni@gmail.com>
2026-07-13 19:34:03 +0530

    chore: update package.json for ios-only support

--- react-native.config.js
+++ react-native.config.js
@@ -2,14 +2,8 @@
  * @type {import('@react-native-community/cli-types').UserDependencyConfig}
  */
 module.exports = {
-  dependency: {
-    platforms: {
-      android: {
-        cmakeListsPath: 'generated/jni/CMakeLists.txt',
-        cxxModuleCMakeListsModuleName: 'react-native-liquid-glass-bottom-tabs',
-        cxxModuleCMakeListsPath: 'CMakeLists.txt',
-        cxxModuleHeaderName: 'LiquidGlassBottomTabsImpl',
-      },
-    },
+  project: {
+    ios: {},
+    // android: {},  <-- Delete or comment this out
   },
 };
