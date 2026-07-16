c49412889646c2c568457005621c31e322e7f052
Neelam Soni <neemssoni@gmail.com>
2026-07-16 10:32:01 +0530

    feat: rewrite bridge using native UITabBarController for Liquid Glass support

--- ios/LiquidTabBarViewManager.swift
+++ ios/LiquidTabBarViewManager.swift
@@ -1,17 +1,12 @@
 import Foundation
 import React
 
-@objc(LiquidTabBarManager)
-class LiquidTabBarManager: RCTViewManager {
+@objc(LiquidTabBarViewManager)
+class LiquidTabBarViewManager: RCTViewManager {
   
   override func view() -> UIView! {
     return LiquidTabBarViewWrapper()
   }
-  
-  @objc var activeTintColor: UIColor {
-          set { /* Not strictly needed if using @objc in the View directly */ }
-          get { return .blue }
-      }
   
   override static func requiresMainQueueSetup() -> Bool {
     return true
